3d5208aff48a720635d64b3b3eabe3f1f6418ab9
Daniel Melchor <danielmelchor@localhost.localdomain>
2021-05-27 14:36:29 +0200

    dani

--- content.js
+++ content.js
@@ -1,81 +1,113 @@
-window.onload = function(){
-    // document.write("Hello World")
+/* global chrome */
+var classesScraped = [];
 
+// Makes the program wait
+const sleep = (ms) => {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+};
 
-    // Checking if JQuery has loaded -- returns function is loaded else undefined
-    // checkForJQuery();
-    // runScript();
+// Redirects User
+const redirect = (to) => {
+  chrome.tabs.getSelected(null, function (tab) {
+    chrome.tabs.update(tab.id, { url: to });
+  });
+};
 
+// Scrapes external credits
+const scrapeExtCredits = () => {
+  chrome.tabs.query(
+    { active: true, windowId: chrome.windows.WINDOW_ID_CURRENT },
+    function (tabs) {
+      tabId = tabs[0].id;
+      let code = `document.body.innerText`;
 
-    console.log(window.location.href);
-    if (window.location.href.includes("allgpa")){
-        console.log("This is the Classes Website");
-    } else if (window.location.href.includes("xcred")){
-        console.log("This is the external credit page");
-    } else{
-        console.log("On BU Page but not correct one");
+      chrome.tabs.executeScript(tabId, { code }, function (result) {
+        let text = result[0];
+
+        let x = text.match(/([A-Z][A-Z][A-Z][A-Z][A-Z][0-9][0-9][0-9])+/g, "");
+
+        classesScraped = classesScraped.concat(x);
+      });
     }
-    runScript();
-}
+  );
+};
 
-function checkForJQuery() {
-    console.log("Checking for JQuery: ");
-    try{
-        console.log(typeof jQuery)
-    } catch (e) {
-        console.log(e);
-    }    
-}
+// Scrapes classes taken
+const scrapeClassesTaken = () => {
+  chrome.tabs.query(
+    { active: true, windowId: chrome.windows.WINDOW_ID_CURRENT },
+    function (tabs) {
+      tabId = tabs[0].id;
+      let code = `document.body.innerText`;
 
-// Runs script after JQuery has been loaded 
-function runScript(){
-    if (window.$){
-        if (window.location.href.includes("allgpa")){
-            scrape_Classes_Taken();
-        } else if (window.location.href.includes("xcred")){
-            scrape_Ext_Credits();
-        } // ADD ELSE CONDITION
-    } else {
-        window.setTimeout(runScript, 100);
+      chrome.tabs.executeScript(tabId, { code }, function (result) {
+        let text = result[0];
+
+        let x = text.match(/([A-Z][A-Z][A-Z] [A-Z][A-Z][0-9][0-9][0-9])+/g, "");
+
+        classesScraped = classesScraped.concat(x);
+      });
     }
-}
+  );
+};
 
+const sendToServer = (classesScraped, buID) => {
+  for (const i in classesScraped)
+    classesScraped[i] = classesScraped[i].replace(" ", "");
 
-// Scrapes external credits 
-function scrape_Ext_Credits(){
-    $.get(window.location.href, function(res){
-        // console.log(res);
-        var table_path = $('body > center > center > table:nth-child(23) > tbody');
-        var num_rows = $('body > center > center > table:nth-child(23) > tbody tr').length;
-        console.log("Num rows: " + num_rows);
+  let data = {
+    classes: classesScraped,
+    buID,
+  };
 
-        for (var i = 6; i < num_rows; i++){
-            var tr = table_path.find('tr:nth-child('+i.toString()+')')
-            var tr_length = table_path.find('tr:nth-child('+i.toString()+') td').length;
-            // console.log(tr.text());
-            for (var j = tr_length; j > tr_length-2; j--){
-                var ext_class = tr.find('td:nth-child('+j.toString()+')')
-                console.log(ext_class.text());
-            }
-        }
-
+  fetch("https://bunexusserver.com/api/saveUserClasses", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data),
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      console.log("Success:", data);
+    })
+    .catch((error) => {
+      console.error("Error:", error);
     });
+};
+
+// The sequence of operations the program runs
+async function scrapeDataSequence() {
+  await chrome.tabs.getSelected(null, async function (tab) {
+    let path = tab.url.split("/");
+    path = path[path.length - 1];
+
+    let privateId = path.split("?")[0];
+
+    redirect(
+      "https://www.bu.edu/link/bin/uiscgi_studentlink.pl/" +
+        privateId +
+        "?ModuleName=allgpa.pl"
+    );
+    await sleep(3000);
+    scrapeClassesTaken();
+
+    redirect(
+      "https://www.bu.edu/link/bin/uiscgi_studentlink.pl/" +
+        privateId +
+        "?ModuleName=xcred.pl"
+    );
+    await sleep(3000);
+    scrapeExtCredits();
+
+    await sleep(1000);
+    sendToServer(classesScraped);
+  });
 }
 
-// Scrapes classes taken 
-function scrape_Classes_Taken(){
-    $.get(window.location.href, function(res){
-        // console.log(res);
-        var table_path = $('body > table:nth-child(5) > tbody');
-        var num_rows = $('body > table:nth-child(5) > tbody tr').length;
-        console.log("Num rows of Classes taken: " + num_rows);
-
-        for (var i = 0; i < num_rows; i++){
-            var tr = table_path.find('tr:nth-child('+i.toString()+')')
-            var exact  = tr.find("td:nth-child(2)")
-            console.log(exact.text());
-        }
-    });
-}
-
-
+// Detects user click
+window.onload = () => {
+  document.getElementById("clickIt").addEventListener("click", () => {
+    scrapeDataSequence();
+  });
+};
