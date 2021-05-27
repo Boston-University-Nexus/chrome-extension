47ea5c3976eb56f3e629e428fb8eb80106d9a952
vineet <rvineet02@gmail.com>
2021-05-27 14:52:26 +0530

    Working for mine external credits

--- content.js
+++ content.js
@@ -29,9 +29,24 @@ function runScript(){
 function scrape(){
     $.get(window.location.href, function(res){
         // console.log(res);
-        var table_path = $('body > center > center > table:nth-child(23) > tbody')
-        var tr = table_path.find('tr:nth-child(6) > td:nth-child(6)')
-        console.log(tr.text());
+        var table_path = $('body > center > center > table:nth-child(23) > tbody');
+        var num_rows = $('body > center > center > table:nth-child(23) > tbody tr').length;
+        console.log("Num rows: " + num_rows);
+
+        for (var i = 6; i < num_rows; i++){
+            var tr = table_path.find('tr:nth-child('+i.toString()+')')
+            var tr_length = table_path.find('tr:nth-child('+i.toString()+') td').length;
+            // console.log(tr.text());
+            for (var j = tr_length; j > tr_length-2; j--){
+                var ext_class = tr.find('td:nth-child('+j.toString()+')')
+                console.log(ext_class.text());
+            }
+        }
+
+
+
+        // var tr = table_path.find('tr:nth-child(6) > td:nth-child(6)')
+        // console.log(tr.text());
 
     });
 }
@@ -42,5 +57,6 @@ function scrape(){
 
 // document.querySelector("body > center > center > table:nth-child(23) > tbody > tr:nth-child(6) > td:nth-child(6) > text")
 
-
+document.querySelector("body > center > center > table:nth-child(23) > tbody > tr:nth-child(6) > td:nth-child(7)")
+document.querySelector("body > center > center > table:nth-child(23) > tbody > tr:nth-child(6)")
 
